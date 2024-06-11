267934f48d5859636b88f4a5d46e2f674aeef53c
lukerpace <lukerpace@gmail.com>
2024-06-11 15:54:29 -0500

    Completed Script.js

--- script.js
+++ script.js
@@ -25,11 +25,37 @@ weatherForm.addEventListener('submit', async (e) => {
 async function getWeatherData(city) {
     const apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
     const response = await fetch(apiurl);
-    console.log(response);
+    if(!response.ok) {
+        throw new Error("City not found");
+    }
+
+     return await response.json();
 }
 
-function displayWeatherData(weatherData) {
+function displayWeatherData(data) {
+    const {name: city, main: {temp, humidity}, weather: [{description, id}]} = data;
+    card.textContent = "";
+    card.style.display = "flex";
+
+    const cityView = document.createElement("h1");
+    const tempView = document.createElement("p");
+    const humidityView = document.createElement("p");
+    const descriptionView = document.createElement("p");
+
+    cityView.textContent = city;
+    tempView.textContent = `Temperature: ${((temp - 273.15) * (9/5) + 32).toFixed(1)}°F`;
+    humidityView.textContent = `Humidity: ${humidity}%`;
+    descriptionView.textContent = `description: ${description}`;
+
+    cityView.classList.add("cityView");
+    tempView.classList.add("tempView");
+    humidityView.classList.add("humidityView");
+    descriptionView.classList.add("descriptionView");
     
+    card.appendChild(cityView);
+    card.appendChild(tempView);
+    card.appendChild(humidityView);
+    card.appendChild(descriptionView);
 }
 
 function displayError(message) {
